d554fcfb588dff115266eb5f7f0cba418134cb74
Ardito Hilman <ardito.hm.work@gmail.com>
2025-06-29 14:44:48 +0700

    feat: add detail drawer to Pages Table for displaying page information

--- app/Livewire/Pages/Table.php
+++ app/Livewire/Pages/Table.php
@@ -5,15 +5,20 @@ namespace App\Livewire\Pages;
 use Livewire\Component;
 use Livewire\Attributes\On; 
 use Mary\Traits\Toast;
+use App\Models\Page;
 
 class Table extends Component
 {
     use Toast;
 
+    public Page $data;
+    public $drawer = false;
+
     #[On('show-detail')] 
     public function detail($id)
     {
-        $this->info('Show details for item with ID: ' . $id);
+        $this->data = Page::findOrFail($id);
+        $this->drawer = true;
     }
 
     public function render()
--- resources/views/livewire/pages/table.blade.php
+++ resources/views/livewire/pages/table.blade.php
@@ -3,4 +3,36 @@
         'model' => 'page',
         'headers' => $headers,
     ])
+
+    <x-mary-drawer
+        wire:model="drawer"
+        title="Page Details"
+        separator
+        with-close-button
+        close-on-escape
+        class="w-11/12 lg:w-1/3"
+        right
+    >
+        @if ($data)
+            <h1 class="font-bold text-2xl lg:text-4xl mb-2">{{ $data->title }}</h1>
+            <div class="flex flex-row justify-between items-center mt-4">
+                <div class="w-2/3 text-sm text-gray-500">
+                    <p>
+                        <span class="font-semibold">Slug:</span> {{ $data->slug }}
+                    </p>
+                    <p>
+                        <span class="font-semibold">Created:</span> {{ $data->created_at->format('d M Y H:i') }}
+                    </p>
+                    <p>
+                        <span class="font-semibold">Updated:</span> {{ $data->updated_at->format('d M Y H:i') }}
+                    </p>
+                </div>
+                <x-mary-badge :value="$data->status"
+                    class="font-bold"
+                    :class=" $data->status === 'published' ? 'badge-success' : 'badge-error'" />
+            </div>
+            
+            <p class="mt-16">{!! $data->body !!}</p>
+        @endif
+    </x-mary-drawer>
 </div>
